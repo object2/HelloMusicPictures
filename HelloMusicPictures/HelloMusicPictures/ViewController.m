a5d10344685714bb57cb4da935cebeabf6a2a712
lukehwang <lukehwang@gmail.com>
2011-11-05 18:55:21 +0900

    사진 라이선스 필터링

--- HelloMusicPictures/HelloMusicPictures/ViewController.m
+++ HelloMusicPictures/HelloMusicPictures/ViewController.m
@@ -290,9 +290,24 @@ NSInteger curPictureIdx = 0;
 
 	NSLog(@"Searching photo with '%@'", keyword);
 	
+	
+	// License
+	// id 4 : Attribution  : 상업적 사용 O, 수정 O, 공유 O
+	// id 6 : A.. NoDerivs : 상업적 사용 O, 수정 X, 공유 O
+	// id 3 : A.. NonComm, NoDrivs : 상업적 사용 X, 수정 X, 공유 O
+	// id 2 : A.. NonComm : 상업적 사용 X, 수정 O, 공유 O
+	// id 1 : A.. NonComm Share : 상업적 사용 X, 수정 O, 공유 O (수정 시 동일 라이선스 유지)
+	// id 5 : A.. Share : 상업적 사용 O, 수정 O, 공유 O (수정 시 동일 라이선스 유지)
+	// id 7 : 라이선스 정보 없음 : public domain?
+	
+	
+	
+	
+	
 	if (![flickrRequest isRunning]) {
 		[flickrRequest callAPIMethodWithGET:@"flickr.photos.search" 
-								  arguments:[NSDictionary dictionaryWithObjectsAndKeys:keyword, @"text", keyword, @"tags", @"20", @"per_page", nil]];
+								  arguments:[NSDictionary dictionaryWithObjectsAndKeys:keyword, @"text", keyword, @"tags",
+											 @"20", @"per_page", @"4, 6, 5, 7", @"license", nil]];
 	}
 }
 
